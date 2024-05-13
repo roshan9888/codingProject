6510364a029688c47a2ebe4c7645beaf4093e5a7
roshan9888 <roshan816000@gmail.com>
2024-05-13 07:55:53 +0530

    comment

--- server/config/database.js
+++ server/config/database.js
@@ -3,27 +3,26 @@ require("dotenv").config();
 
 const { MONGODB_URL } = process.env;
 
-// exports.connect = () => {
-// 	mongoose
-// 		.connect(MONGODB_URL, {
-// 			useNewUrlparser: true,
-// 			useUnifiedTopology: true,
-// 		})
-		// .then(console.log(`DB Connection Success`))
-		// .catch((err) => {
-		// 	console.log(`DB Connection Failed`);
-		// 	console.log(err);
-		// 	process.exit(1);
-		// });
-// };
-
-
 exports.connect = () => {
-	mongoose.connect(process.env.MONGODB_URL)
-	.then(console.log(`DB Connection Success`))
+	mongoose
+		.connect(MONGODB_URL, {
+			useNewUrlparser: true,
+			useUnifiedTopology: true,
+		})
+		.then(console.log(`DB Connection Success`))
 		.catch((err) => {
 			console.log(`DB Connection Failed`);
 			console.log(err);
 			process.exit(1);
 		});
-}
+};
+
+// exports.connect = () => {
+// 	mongoose.connect(process.env.MONGODB_URL)
+// 	.then(console.log(`DB Connection Success`))
+// 		.catch((err) => {
+// 			console.log(`DB Connection Failed`);
+// 			console.log(err);
+// 			process.exit(1);
+// 		});
+// }
