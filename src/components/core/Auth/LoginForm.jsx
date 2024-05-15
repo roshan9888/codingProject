cc05d6851e59468ed7f91c9e3955d4f2f6a316df
roshan9888 <roshan816000@gmail.com>
2024-05-15 19:25:19 +0530

    Password to Forgot Password

--- src/components/core/Auth/LoginForm.jsx
+++ src/components/core/Auth/LoginForm.jsx
@@ -73,7 +73,7 @@ function LoginForm() {
         </span>
         <Link to="/forgot-password">
           <p className="mt-1 ml-auto max-w-max text-xs text-blue-100">
-            Forgot 
+            Forgot Password
           </p>
         </Link>
       </label>
